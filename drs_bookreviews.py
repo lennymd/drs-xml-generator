5538fda204da850a826fc6751f18b80f079974f3
Lenny Martinez Dominguez <lennymartinezd@gmail.com>
2025-08-07 16:18:04 +0200

    work on files

---
+++ drs_bookreviews.py
@@ -0,0 +1,33 @@
+from drsxml.core import (
+    generate_doi_batch_root,
+    populate_doi_batch_head,
+    populate_book_review_body,
+    load_xls_data,
+    write_xml_to_file,
+)
+
+# LOAD XLS FILE FOR PROCESSING
+
+FILE = "books0805.xls"
+
+records = load_xls_data(FILE)
+
+# PREVIEW RECORDS
+# for rec in records[:5]:
+#     print(rec)
+
+
+# GENERATE XML TREE
+
+# Create the root element for the DOI batch
+root, head, body = generate_doi_batch_root()
+
+# Populate the head with metadata
+populate_doi_batch_head(head, FILE)
+
+# Populate the body with book review records
+populate_book_review_body(body, records)
+
+# Save XML to file
+
+write_xml_to_file(root, "book-reviews.xml")
---
+++ drs_conference.py
@@ -0,0 +1,41 @@
+from drsxml.core import (
+    generate_doi_batch_root,
+    populate_doi_batch_head,
+    preview_xml_tree,
+    load_xls_data,
+    write_xml_to_file,
+    populate_proceedings_body,
+)
+
+# Define the conference information
+conf_info = {
+    "data_file": "25.xls",
+    "name": "Nordes 2025: Relational Design",
+    "acronym": "Nordes 2025",
+    "volume_doi": "10.21606/nordes.2025.cv",
+    "volume_url": "https://dl.designresearchsociety.org/conference-volumes/67/",
+    "issn": "16049705",
+    "isbn": "",
+    "series_title": "Nordic Design Research Conference",
+}
+
+OUT_FILE = "250805-nordes2025.xml"
+
+# Load the data from the XLS file
+records = load_xls_data(conf_info.get("data_file"))
+
+
+# GENERATE XML TREE
+
+# Create the root element for the DOI batch
+root, head, body = generate_doi_batch_root()
+
+# Populate the head with metadata
+populate_doi_batch_head(head, conf_info.get("data_file"))
+
+body.clear()
+
+# Populate the body with book review records
+populate_proceedings_body(body, records, conf_info)
+
+write_xml_to_file(root, OUT_FILE)
